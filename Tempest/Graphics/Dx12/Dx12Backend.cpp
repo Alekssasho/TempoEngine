b58c048c38f270227ce87aa5c8b2287ce14ce30f
Alekssasho <aleksandar.angelovv@gmail.com>
2020-07-28 10:58:58 +0300

    Added test usage of flatbuffers.

--- Tempest/Graphics/Dx12/Dx12Backend.cpp
+++ Tempest/Graphics/Dx12/Dx12Backend.cpp
@@ -5,6 +5,10 @@
 #include <dxgidebug.h>
 #endif
 
+#include <fstream>
+#include <DataDefinitions/ShaderLibrary_generated.h>
+
+
 namespace Tempest
 {
 namespace Dx12
@@ -32,9 +36,117 @@ void Backend::Initialize(WindowHandle handle)
 {
 	m_Device->Initialize(handle);
 
-	auto desc = m_PipelineManager.PrepareDefaultPipelineStateDesc();
+	// Load Shader Library
+	std::ifstream stream("../../Tempest/Shaders/ShaderLibrary.tslb", std::ios::binary);
+	assert(stream.good());
+	stream.seekg(0, std::ios::end);
+	size_t size = stream.tellg();
+	stream.seekg(0);
 
-	//m_MeshPipelineHandle = m_PipelineManager.CreateGraphicsPipeline(desc);
+	eastl::vector<uint8_t> data(size);
+	stream.read((char*)data.data(), size);
+
+	const Definition::ShaderLibrary* shaderLib = Definition::GetShaderLibrary(data.data());
+	flatbuffers::Verifier verifier(data.data(), data.size());
+	assert(shaderLib->Verify(verifier));
+
+	auto shaders = shaderLib->Shaders();
+	eastl::vector<const char*> shaderNames;
+	for (auto sh : *shaders) {
+		shaderNames.push_back(sh->Name()->c_str());
+	}
+	auto uiVsShader = shaders->LookupByKey("UI-VS");
+	assert(uiVsShader);
+	auto uiPsShader = shaders->LookupByKey("UI-PS");
+	assert(uiPsShader);
+	assert(uiVsShader->Type() == Definition::ShaderType_Vertex);
+	assert(uiPsShader->Type() == Definition::ShaderType_Pixel);
+
+	auto desc = m_PipelineManager.PrepareDefaultPipelineStateDesc();
+	desc.VS.pShaderBytecode = uiVsShader->Code()->Data();
+	desc.VS.BytecodeLength = uiVsShader->Code()->size();
+	desc.PS.pShaderBytecode = uiPsShader->Code()->Data();
+	desc.PS.BytecodeLength = uiPsShader->Code()->size();
+
+	D3D12_ROOT_PARAMETER rootParam;
+	rootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+	rootParam.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	rootParam.Constants.Num32BitValues = 4;
+	rootParam.Constants.RegisterSpace = 0;
+	rootParam.Constants.ShaderRegister = 0;
+
+	D3D12_DESCRIPTOR_RANGE range;
+	range.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	range.NumDescriptors = 1;
+	range.BaseShaderRegister = 0;
+	range.RegisterSpace = 0;
+	range.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+	D3D12_ROOT_PARAMETER rootParam2;
+	rootParam2.ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+	rootParam2.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	rootParam2.DescriptorTable.NumDescriptorRanges = 1;
+	rootParam2.DescriptorTable.pDescriptorRanges = &range;
+
+	D3D12_STATIC_SAMPLER_DESC sampler;
+	sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+	sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	sampler.MipLODBias = 0;
+	sampler.MaxAnisotropy = 0;
+	sampler.MinLOD = 0;
+	sampler.MaxLOD = 0;
+	sampler.ShaderRegister = 0;
+	sampler.RegisterSpace = 0;
+	sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+
+	D3D12_ROOT_PARAMETER params[2] = { rootParam, rootParam2 };
+	D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+	rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	rootSignatureDesc.NumParameters = 2;
+	rootSignatureDesc.pParameters = params;
+	rootSignatureDesc.NumStaticSamplers = 1;
+	rootSignatureDesc.pStaticSamplers = &sampler;
+
+	ComPtr<ID3DBlob> signature;
+	ComPtr<ID3DBlob> error;
+	ComPtr<ID3D12RootSignature> rootSignature;
+	CHECK_SUCCESS(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+
+	CHECK_SUCCESS(m_Device->GetDevice()->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&rootSignature)));
+	desc.pRootSignature = rootSignature.Get();
+
+	eastl::vector<D3D12_INPUT_ELEMENT_DESC> inputElements;
+	inputElements.resize(3);
+	inputElements[0].SemanticName = "POSITION";
+	inputElements[0].SemanticIndex = 0;
+	inputElements[0].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElements[0].InputSlot = 0;
+	inputElements[0].AlignedByteOffset = 0;
+	inputElements[0].InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+	inputElements[0].InstanceDataStepRate = 0;
+
+	inputElements[1].SemanticName = "TEXCOORD";
+	inputElements[1].SemanticIndex = 0;
+	inputElements[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElements[1].InputSlot = 0;
+	inputElements[1].AlignedByteOffset = 2 * sizeof(float);
+	inputElements[1].InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+	inputElements[1].InstanceDataStepRate = 0;
+
+	inputElements[2].SemanticName = "COLOR";
+	inputElements[2].SemanticIndex = 0;
+	inputElements[2].Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	inputElements[2].InputSlot = 0;
+	inputElements[2].AlignedByteOffset = 4 * sizeof(float);
+	inputElements[2].InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+	inputElements[2].InstanceDataStepRate = 0;
+
+	desc.InputLayout.pInputElementDescs = inputElements.data();
+	desc.InputLayout.NumElements = UINT(inputElements.size());
+
+	m_MeshPipelineHandle = m_PipelineManager.CreateGraphicsPipeline(desc);
 }
 
 void Backend::RenderFrame()
--- Thunder/ShaderCompiler/src/main.rs
+++ Thunder/ShaderCompiler/src/main.rs
@@ -1,4 +1,5 @@
-extern crate hassle_rs;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -15,11 +16,39 @@ fn compile_hlsl_source_file(source_name: &str, shader_type: ShaderType) -> Optio
         ShaderType::PixelShader => ("ps_6_0", "PixelShaderMain"),
     };
 
-    if let Ok(code) = hassle_rs::compile_hlsl(source_name, &shader_text, entry_point, target_profile, &[], &[]) {
-        Some(code)
-    } else {
-        None
+    let result = hassle_rs::compile_hlsl(
+        source_name,
+        &shader_text,
+        entry_point,
+        target_profile,
+        &[],
+        &[],
+    );
+    match result {
+        Ok(code) => Some(code),
+        Err(err) => panic!("Error {}", err),
     }
+}
+
+#[allow(dead_code)]
+fn write_bytecode_to_header(source: Vec<u8>, output_file: PathBuf, shader_type: ShaderType) {
+    let mut file = File::create(output_file).expect("Cannot create output file!");
+    let mut contents = String::with_capacity(1024);
+    let variable_name = match shader_type {
+        ShaderType::VertexShader => "g_VertexShaderMain[]",
+        ShaderType::PixelShader => "g_PixelShaderMain[]",
+    };
+    contents.push_str("const unsigned char ");
+    contents.push_str(variable_name);
+    contents.push_str(" = {\n");
+    for byte in source {
+        contents.push_str(&byte.to_string());
+        contents.push_str(",");
+    }
+    contents.pop(); // remove the last comma
+    contents.push_str("\n};\n");
+    file.write_all(contents.as_bytes())
+        .expect("Cannot write into file");
 }
 
 #[derive(StructOpt)]
@@ -52,8 +81,63 @@ fn main() {
     let input_shaders =
         read_input_files(&opt.input_folder).expect("Cannot read input folder for shaders");
 
+    let mut shader_library_builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024 * 1024);
+    let mut shaders_vector = Vec::with_capacity(input_shaders.len() * 2);
     for shader in input_shaders {
-        let compiled_vertex_shader = compile_hlsl_source_file(shader.to_str().unwrap(), ShaderType::VertexShader);
-        let compiled_pixel_shader = compile_hlsl_source_file(shader.to_str().unwrap(), ShaderType::PixelShader);
+        {
+            let compiled_vertex_shader =
+                compile_hlsl_source_file(shader.to_str().unwrap(), ShaderType::VertexShader);
+            let name = format!("{}-VS", shader.file_stem().unwrap().to_str().unwrap());
+            let vertex_shader_name = shader_library_builder.create_string(&name);
+            let vertex_code = shader_library_builder.create_vector(&compiled_vertex_shader.unwrap()[..]);
+            let vertex_shader = data_definition_generated::Shader::create(&mut shader_library_builder, &data_definition_generated::ShaderArgs{
+                Name: Some(vertex_shader_name),
+                Type: data_definition_generated::ShaderType::Vertex,
+                Code: Some(vertex_code),
+            });
+            shaders_vector.push((name, vertex_shader));
+
+            // let mut output_file = shader.clone();
+            // output_file.set_file_name(format!("{}-VS", output_file.file_stem().unwrap().to_str().unwrap()));
+            // output_file.set_extension("h");
+            // println!("Outputing vertex shader code to {:?}", output_file);
+            // write_bytecode_to_header(compiled_vertex_shader.unwrap(), output_file, ShaderType::VertexShader);
+        }
+
+        {
+            let compiled_pixel_shader =
+                compile_hlsl_source_file(shader.to_str().unwrap(), ShaderType::PixelShader);
+
+            let name = format!("{}-PS", shader.file_stem().unwrap().to_str().unwrap());
+            let pixel_shader_name = shader_library_builder.create_string(&name);
+            let pixel_code = shader_library_builder.create_vector(&compiled_pixel_shader.unwrap()[..]);
+            let pixel_shader = data_definition_generated::Shader::create(&mut shader_library_builder, &data_definition_generated::ShaderArgs{
+                Name: Some(pixel_shader_name),
+                Type: data_definition_generated::ShaderType::Pixel,
+                Code: Some(pixel_code),
+            });
+            shaders_vector.push((name, pixel_shader));
+            // let mut output_file = shader.clone();
+            // output_file.set_file_name(format!("{}-PS", output_file.file_stem().unwrap().to_str().unwrap()));
+            // output_file.set_extension("h");
+            // println!("Outputing pixel shader code to {:?}", output_file);
+            // write_bytecode_to_header(compiled_vertex_shader.unwrap(), output_file, ShaderType::PixelShader);
+        }
     }
+
+    shaders_vector.sort_unstable_by(|lhs, rhs| lhs.0.cmp(&rhs.0));
+    let sorted_vector: Vec<flatbuffers::WIPOffset<data_definition_generated::Shader>> = shaders_vector.into_iter().map(|x| x.1).collect();
+
+    let shaders = shader_library_builder.create_vector(&sorted_vector[..]);
+    let root_shader_library = data_definition_generated::ShaderLibrary::create(&mut shader_library_builder, &data_definition_generated::ShaderLibraryArgs{
+        Shaders: Some(shaders)
+    });
+    shader_library_builder.finish(root_shader_library, Some(data_definition_generated::SHADER_LIBRARY_IDENTIFIER));
+    let data = shader_library_builder.finished_data();
+
+    let mut output_file_path = opt.output_folder;
+    output_file_path.push("ShaderLibrary");
+    output_file_path.set_extension(data_definition_generated::SHADER_LIBRARY_EXTENSION);
+    let mut output_file = File::create(output_file_path).expect("Cannot create output file");
+    output_file.write_all(data).expect("Cannot write output data");
 }

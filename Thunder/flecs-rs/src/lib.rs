f9aba825f6cc8973f4222374a4e8bd10553cb261
Alekssasho <aleksandar.angelovv@gmail.com>
2021-10-12 10:26:55 +0300

    Cleanup rust code for flecs cooking

--- Thunder/flecs-rs/src/lib.rs
+++ Thunder/flecs-rs/src/lib.rs
@@ -7,59 +7,12 @@ include!(concat!(env!("OUT_DIR"), "/flecs-binding.rs"));
 extern crate flecs_rs_derive;
 
 use components::*;
-use std::{
-    collections::HashMap,
-    ffi::{c_void, CStr, CString},
-    sync::Arc,
-};
+use std::collections::HashMap;
 
-fn cstr(input: &str) -> CString {
-    CString::new(input).expect("CString::new failed")
-}
 pub struct FlecsState {
-    world: *mut ecs_world_t,
-    component_entities: Vec<(ecs_entity_t, u64)>,
-    tag_entities: Vec<ecs_entity_t>,
-
     // Bulk API
     next_entity_id: u64,
     archetypes: HashMap<String, Vec<Vec<Components>>>,
-}
-
-impl Drop for FlecsState {
-    fn drop(&mut self) {
-        // unsafe {
-        //     ecs_fini(self.world);
-        // }
-    }
-}
-
-fn register_tag(world: *mut ecs_world_t, name: &[u8]) -> ecs_entity_t {
-    unsafe {
-        // ecs_new_entity(
-        //     world,
-        //     0,
-        //     CStr::from_bytes_with_nul(name).unwrap().as_ptr(),
-        //     std::ptr::null(),
-        // )
-        0
-    }
-}
-
-fn register_component<T>(world: *mut ecs_world_t, name: &[u8]) -> (ecs_entity_t, u64) {
-    unsafe {
-        (
-            // ecs_new_component(
-            //     world,
-            //     0,
-            //     CStr::from_bytes_with_nul(name).unwrap().as_ptr(),
-            //     std::mem::size_of::<T>() as u64,
-            //     std::mem::align_of::<T>() as u64,
-            // ),
-            1,
-            std::mem::size_of::<T>() as u64,
-        )
-    }
 }
 
 #[derive(Components)]
@@ -81,36 +34,16 @@ pub enum Tags {
 }
 
 impl FlecsState {
-    fn get_component_entity(&self, component: &Components) -> (ecs_entity_t, u64) {
-        self.component_entities[component.get_index()]
-    }
-
-    fn get_tag_entity(&self, tag: &Tags) -> ecs_entity_t {
-        self.tag_entities[tag.get_index()]
-    }
-
     pub fn new() -> Self {
-        unsafe {
-            //let world = ecs_init();
-            let world = std::ptr::null_mut();
-            // Register all components / tags
-            let component_entities = Components::register_components(world);
-            let tag_entities = Tags::register_tags(world);
-
-            FlecsState {
-                world,
-                component_entities,
-                tag_entities,
-
-                next_entity_id: 0,
-                archetypes: Default::default(),
-            }
+        FlecsState {
+            next_entity_id: 0,
+            archetypes: Default::default(),
         }
     }
 
     pub fn create_entity(
         &mut self,
-        name: &str,
+        _name: &str, // TODO: Find a way to add names
         components: Vec<Components>,
         tags: &[Tags],
     ) -> ecs_entity_t {
@@ -144,18 +77,24 @@ impl FlecsState {
 
     pub fn write_to_buffer<W: std::io::Write>(&self, writer: &mut W) {
         // Write number of archetypes
-        writer.write_all(&u32::to_ne_bytes(self.archetypes.len() as u32)).unwrap();
+        writer
+            .write_all(&u32::to_ne_bytes(self.archetypes.len() as u32))
+            .unwrap();
 
         for (name, components_array) in self.archetypes.iter() {
             write!(writer, "{}\0", name).unwrap();
             // Number of components
-            writer.write_all(&u32::to_ne_bytes(components_array.first().unwrap().len() as u32)).unwrap();
+            writer
+                .write_all(&u32::to_ne_bytes(
+                    components_array.first().unwrap().len() as u32
+                ))
+                .unwrap();
             for array in components_array {
                 for component in array {
-                    let (ptr, size) = component.get_pointer();
+                    let (ptr, size) = component.get_pointer_and_size();
                     unsafe {
                         writer
-                            .write_all(std::slice::from_raw_parts(ptr as *const u8, size))
+                            .write_all(std::slice::from_raw_parts(ptr, size))
                             .unwrap();
                     }
                 }
@@ -163,21 +102,5 @@ impl FlecsState {
         }
 
         writer.flush().expect("Cannot flush");
-
-        // unsafe {
-        //     let mut reader = ecs_reader_init(self.world);
-        //     const buffer_size: i32 = 256;
-        //     let mut buff = [0u8; buffer_size as usize];
-        //     let mut read = 1;
-        //     while read > 0 {
-        //         read = ecs_reader_read(
-        //             &mut buff as *mut _ as *mut ::std::os::raw::c_char,
-        //             buffer_size,
-        //             &mut reader,
-        //         );
-        //         writer.write_all(&buff).expect("Cannot write into writer");
-        //     }
-        //     writer.flush().expect("Cannot flush");
-        // }
     }
 }
